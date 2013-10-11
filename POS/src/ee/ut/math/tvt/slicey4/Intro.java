6f53a50ca822765601e60c4feefa524a808b8f58
Slicey4 <katlinviilukas@gmail.com>
2013-10-11 15:13:11 +0300

    katse3

---
+++ POS/src/ee/ut/math/tvt/slicey4/Intro.java
@@ -0,0 +1,9 @@
+package ee.ut.math.tvt.slicey4;
+
+import javax.swing.JFrame;
+
+public class Intro extends JFrame{
+public static void main(String[] args) {
+		
+	}
+}
---
+++ POS/src/ee/ut/math/tvt/slicey4/IntroUI.java
@@ -0,0 +1,25 @@
+package ee.ut.math.tvt.slicey4;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class IntroUI extends Intro{
+	public static String Team_name="Slicey4";
+	public static String Team_leader= "Kätlin Viilukas";
+	public static String Team_leader_email= "katlinviilukas@gmail.com";
+	public static String[] Team_members= {"Kätlin Viilukas", "Jane Jürgenson", "Kristiina Pokk", "Linda-Liis Eek"};
+	public static Icon Team_logo;
+	public static int software_version_number;
+
+	
+	void Loo(){
+	JFrame frame = new JFrame("no image");
+	ImageIcon image = new ImageIcon("logo.png");
+	JLabel imagelabel = new JLabel(image);
+	frame.add(imagelabel);
+	
+	}
+	
+}

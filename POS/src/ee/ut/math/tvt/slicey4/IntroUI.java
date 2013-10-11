6482097b4e792622c0992ba7b26d54bd7d13ae40
Slicey4 <katlinviilukas@gmail.com>
2013-10-11 17:52:02 +0300

    Failist lugemine

--- POS/src/ee/ut/math/tvt/slicey4/IntroUI.java
+++ POS/src/ee/ut/math/tvt/slicey4/IntroUI.java
@@ -1,5 +1,15 @@
 package ee.ut.math.tvt.slicey4;
 import java.awt.GridLayout;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -8,20 +18,20 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class IntroUI extends Intro{
-	public static String Team_name;
-	public static String Team_leader;
-	public static String Team_leader_email;
-	public static String[] Team_members;
+public class IntroUI extends JFrame{
+
 	public static Icon Team_logo;
-	public static int software_version_number;
+	
 	
 	public JFrame window;
 	public JLabel Imagelabel;
 	public JPanel panel;
 
 	
-	void Create(){
+				
+	
+	void Create(Map<String, String> m){
+		
 		JFrame frame = new JFrame("Intro");
 		frame.setLayout(new GridLayout(1,2));
     	frame.setSize(1400,600);
@@ -35,11 +45,11 @@ public class IntroUI extends Intro{
         
 		panel = new JPanel(new GridLayout(5,1));
 		
-		JLabel text1 = new JLabel("Team name:" + Team_name);
-		JLabel text2 = new JLabel("Team leader: " + Team_leader);
-		JLabel text3 = new JLabel("Team leader email: " + Team_leader_email);
-		JLabel text4 = new JLabel("Team members: " + Team_members.toString());
-		JLabel text5 = new JLabel("Software version number: " + software_version_number);
+		JLabel text1 = new JLabel("Team name:" + m.get("team.name"));
+		JLabel text2 = new JLabel("Team leader: " + m.get("team.leader"));
+		JLabel text3 = new JLabel("Team leader email: " + m.get("leader.email"));
+		JLabel text4 = new JLabel("Team members: " + m.get("team.members"));
+		JLabel text5 = new JLabel("Software version number: " + m.get("versioon"));
 		
 		panel.add(text1);
 		panel.add(text2);
